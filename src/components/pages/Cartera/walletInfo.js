8717f3ded89b8295117cad60c35d58d57cc785de
jchapan <jchapan@gmail.com>
2020-07-12 15:50:35 -0500

    api

--- src/components/pages/Cartera/walletInfo.js
+++ src/components/pages/Cartera/walletInfo.js
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function WalletInfo(props) {
-    const URLW=`https://apicrypto-cinta-roja.firebaseio.com/crypto.json`
+    const URLW=`https://apiinfowallet.firebaseio.com/.json`
     const [walletCoins, setWalletCoins] = useState([])
 
     useEffect (()=>{
         axios.get(URLW)
         .then((response)=>{
-            let allWalletCoins = response.data
+            let allWalletCoins = response.data[0].coins
             setWalletCoins(allWalletCoins)
             console.log(allWalletCoins)
         }) .catch((err)=>{
@@ -21,7 +21,7 @@ function WalletInfo(props) {
         {
             walletCoins.map((cantidad)=>{
                 return props.moneda == cantidad.name ?
-                 parseInt(cantidad.amount) :undefined
+                 cantidad.amount :undefined
             })
         }
         </>
