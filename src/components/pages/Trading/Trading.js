dfa378446261046a24f3e7e49c8955523140b18f
danysalame18 <65205086+danysalame18@users.noreply.github.com>
2020-07-13 14:08:53 -0500

    tradig

--- src/components/pages/Trading/Trading.js
+++ src/components/pages/Trading/Trading.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Cartera from '../Cartera/Cartera'
 import Header from '../../layout/header/Header'
 import Footer from '../../layout/footer/Footer'
 import './Trading.css'
+import axios from 'axios'
+
 
 function Trading() {
   const [coinSell, setCoinSell] = useState('')
   const [coinBuy, setCoinBuy] = useState('')
+
+  useEffect(()=>{
+    getSetCoin()
+})
+const getSetCoin = ()=>{
+  axios.get(`https://apiinfowallet.firebaseio.com/.json`)
+      .then((response)=>{
+          let amountSellCoin = response.data.amount[0]
+          console.log(amountSellCoin)
+          setCoinSell(amountSellCoin)
+          return amountSellCoin
+      })
+      .catch((error)=>{console.log(error)})
+}
 
   const ejecutar = (event) =>{
     event.preventDefault();
@@ -18,8 +34,7 @@ function Trading() {
     <Cartera />
     <br />
     <div className="container">
-      <div className="col-sm-6">
-        <div className="card">
+        <div className="card col-sm-6">
           <div className="card-body">
       <h3>Buy / Sell Crypto</h3>
             <form onSubmit = {ejecutar}>
@@ -31,20 +46,21 @@ function Trading() {
                 onChange= {(e)=>{setCoinSell(e.target.value)}}
                 >
                     <option value="0" >Select a Coin</option>
-                    <option value="1">BTC</option>
-                    <option value="2">ETH</option>
-                    <option value="3">XRP</option>
-                    <option value="4">USDT</option>
-                    <option value="5">BCH</option>
+                    <option value="BTC">BTC2</option>
+                    <option value="ETH">ETH</option>
+                    <option value="XRP">XRP</option>
+                    <option value="USDT">USDT</option>
+                    <option value="BCH">BCH</option>
                 </select>
+                <span className="input-group-text">{coinSell}</span>
+                {/* <input placeholder="Pon un numero" type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></input> */}
                 <br />
             </form>
             </div>
             </div>
             </div>
-            <div className="container"></div>
-      <div className="col-sm-6">
-        <div className="card">
+            <div className="container">
+        <div className="card col-sm-6">
           <div className="card-body">
       <h3>Buy / Sell Crypto</h3>
             <form onSubmit = {ejecutar}>
@@ -68,7 +84,6 @@ function Trading() {
                 <br />
                 
             </form>
-            </div>
             </div>
             </div>
             </div>
